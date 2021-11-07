376e43c4cd054c287eb125e6803e1d14670d48c3
Akhil2193 <bansalakhil19@gmail.com>
2021-11-07 14:04:29 +0530

    fix algorithm error

--- src/main/java/com/example/food/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/example/food/filter/CustomAuthenticationFilter.java
@@ -51,7 +51,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
         User user = (User)authentication.getPrincipal();
 
-        Algorithm algorithm = Algorithm.HMAC256("Secret".getBytes());
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
 
         String access_token = JWT.create()
                 .withSubject(user.getUsername())
