5aeb6426b2dfaf9985b0442990bb50501e5fe490
Akhil2193 <bansalakhil19@gmail.com>
2021-11-21 15:46:31 +0530

    update app name and add image path to footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <div>
             <section className="footer">
-                <p>&copy;<span> foodizone </span>is a Food Take-Away Web Application.</p>
+                <p>&copy;<span> Hungrezy </span>is a Food Take-Away Web Application.</p>
                  <p>
                     Made for the Partial Fulfillment of Object Oriented Programming Course (CS F213)
                 </p><a href="https://github.com/apupneja/food-takeaway-application" >View Repository</a>
