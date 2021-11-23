8eb4170205d194bd22ca64dedd56c3e17e71274d
imnotgivingup <praneethb2002@gmail.com>
2021-11-23 19:04:44 +0530

    SQl order table fix

--- server/src/main/java/com/example/food/models/Orders.java
+++ server/src/main/java/com/example/food/models/Orders.java
@@ -1,16 +1,29 @@
 package com.example.food.models;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
-public class Order {
+public class Orders {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long restaurantId;
     private String username;
     @ManyToMany(cascade = {CascadeType.ALL})
     private List<FoodOrder> foodItemOrder;
+
+    public String getUsername(){
+        return username;
+    }
+
+    public long getRestaurantId(){
+        return restaurantId;
+    }
+
+    public List<FoodOrder> getFoodItemOrder(){
+        return foodItemOrder;
+    }
 
 
 }
--- server/src/main/java/com/example/food/repository/OrderRepository.java
+++ server/src/main/java/com/example/food/repository/OrderRepository.java
@@ -1,11 +1,11 @@
 package com.example.food.repository;
 
 import com.example.food.models.FoodOrder;
-import com.example.food.models.Order;
+import com.example.food.models.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Orders, Long> {
 
 }
